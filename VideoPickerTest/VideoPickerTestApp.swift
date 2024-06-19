20a11553356c9ea464c2319110bd6816287e2dd0
yukiny0811 <yukiny0811@gmail.com>
2024-06-19 18:41:37 +0900

    Initial Commit

---
+++ VideoPickerTest/VideoPickerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPickerTestApp.swift
+//  VideoPickerTest
+//
+//  Created by Yuki Kuwashima on 2024/06/19.
+//
+
+import SwiftUI
+
+@main
+struct VideoPickerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

1e1c8345eb0b24fe2509466fb3e9284b0005177e
yukiny0811 <yukiny0811@gmail.com>
2024-06-19 18:49:57 +0900

    a

--- VideoPickerTest/ContentView.swift
+++ VideoPickerTest/ContentView.swift
@@ -6,16 +6,40 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+
+    @State private var selectedVideo: [PhotosPickerItem] = []
+    @State private var videoURL: URL?
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        PhotosPicker("写真を選択", selection: $selectedVideo, maxSelectionCount: 1, matching: .any(of: [.videos]), photoLibrary: .shared())
+            .onChange(of: selectedVideo) {
+                Task {
+                    videoURL = await getVideoURL(videos: selectedVideo)
+                    print(videoURL)
+                }
+            }
+    }
+
+    private func getVideoURL(videos: [PhotosPickerItem]) async -> URL? {
+        guard let video = videos.first else {
+            return nil
         }
-        .padding()
+        guard let data = try? await video.loadTransferable(type: Data.self) else {
+            return nil
+        }
+        guard let tempDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        let fileURL = tempDirectory.appendingPathComponent(UUID().uuidString).appendingPathExtension("mov")
+        do {
+            try data.write(to: fileURL)
+            return fileURL
+        } catch {
+            return nil
+        }
     }
 }
 
